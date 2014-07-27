56c5b503edc2642dc83c968ed906e5db881cb0a2
Greg Allen <me@jga.me>
2014-07-27 15:33:12 -0700

    Release 0.0.1

---
+++ dist/formjax.bower.js
@@ -0,0 +1,71 @@
+/*!
+ * formjax - Submit a form via ajax
+ * v0.0.1
+ * 
+ * copyright First+Third 2014
+ * MIT License
+*/
+(function($) {
+  $.declare('formjax', {
+
+    defaults: {
+      submitButton: '[type=submit]',
+      progressClass: 'btn-progress',
+      successClass: 'btn-success',
+      errorClass: 'btn-error',
+      progressText: 'Sending...',
+      successText: 'Done!',
+      errorText: 'Uh oh',
+      validate: function() {
+        //do some validation, if return false, don't submit, if true, submit
+        return true;
+      },
+      success: function(response) {
+        //fired when form response comes back
+      },
+      error: function(response) {
+        //fired when form errored out
+      }
+    },
+
+    events: {
+      'submit': 'submit'
+    },
+
+    init: function() {
+      this.button = this.el.find(this.submitButton);
+    },
+
+    submit: function(e) {
+      e.preventDefault();
+      if(this.validate()) {
+        this.button
+          .addClass(this.progressClass)
+          .val(this.progressText);
+
+        $.post(this.el.attr('action'), this.el.serialize())
+          .done(this.proxy(this._success))
+          .fail(this.proxy(this._error));
+      }
+    },
+
+    _success: function(response) {
+      this.button
+        .removeClass(this.progressClass)
+        .addClass(this.successClass)
+        .val(this.successText);
+      this.emit('ajaxify:success', response);
+      this.success.call(this.el, response);
+    },
+
+    _error: function(response) {
+      this.button
+        .removeClass(this.progressClass)
+        .addClass(this.errorClass)
+        .val(this.errorText);
+      $.notice(response.responseJSON.message, { level: 'error' });
+      this.emit('ajaxify:error', response);
+      this.error.call(this.el, response);
+    }
+  });
+})(jQuery);
---
+++ dist/formjax.js
@@ -0,0 +1,261 @@
+/*!
+ * formjax - Submit a form via ajax
+ * v0.0.1
+ * 
+ * copyright First+Third 2014
+ * MIT License
+*/
+/*!
+ * fidel - a ui view controller
+ * v2.2.5
+ * https://github.com/jgallen23/fidel
+ * copyright Greg Allen 2014
+ * MIT License
+*/
+(function(w, $) {
+  var _id = 0;
+  var Fidel = function(obj) {
+    this.obj = obj;
+  };
+
+  Fidel.prototype.__init = function(options) {
+    $.extend(this, this.obj);
+    this.id = _id++;
+    this.namespace = '.fidel' + this.id;
+    this.obj.defaults = this.obj.defaults || {};
+    $.extend(this, this.obj.defaults, options);
+    $('body').trigger('FidelPreInit', this);
+    this.setElement(this.el || $('<div/>'));
+    if (this.init) {
+      this.init();
+    }
+    $('body').trigger('FidelPostInit', this);
+  };
+  Fidel.prototype.eventSplitter = /^(\w+)\s*(.*)$/;
+
+  Fidel.prototype.setElement = function(el) {
+    this.el = el;
+    this.getElements();
+    this.dataElements();
+    this.delegateEvents();
+    this.delegateActions();
+  };
+
+  Fidel.prototype.find = function(selector) {
+    return this.el.find(selector);
+  };
+
+  Fidel.prototype.proxy = function(func) {
+    return $.proxy(func, this);
+  };
+
+  Fidel.prototype.getElements = function() {
+    if (!this.elements)
+      return;
+
+    for (var selector in this.elements) {
+      var elemName = this.elements[selector];
+      this[elemName] = this.find(selector);
+    }
+  };
+
+  Fidel.prototype.dataElements = function() {
+    var self = this;
+    this.find('[data-element]').each(function(index, item) {
+      var el = $(item);
+      var name = el.data('element');
+      self[name] = el;
+    });
+  };
+
+  Fidel.prototype.delegateEvents = function() {
+    if (!this.events)
+      return;
+    for (var key in this.events) {
+      var methodName = this.events[key];
+      var match = key.match(this.eventSplitter);
+      var eventName = match[1], selector = match[2];
+
+      var method = this.proxy(this[methodName]);
+
+      if (selector === '') {
+        this.el.on(eventName + this.namespace, method);
+      } else {
+        if (this[selector] && typeof this[selector] != 'function') {
+          this[selector].on(eventName + this.namespace, method);
+        } else {
+          this.el.on(eventName + this.namespace, selector, method);
+        }
+      }
+    }
+  };
+
+  Fidel.prototype.delegateActions = function() {
+    var self = this;
+    self.el.on('click'+this.namespace, '[data-action]', function(e) {
+      var el = $(this);
+      var action = el.attr('data-action');
+      if (self[action]) {
+        self[action](e, el);
+      }
+    });
+  };
+
+  Fidel.prototype.on = function(eventName, cb) {
+    this.el.on(eventName+this.namespace, cb);
+  };
+
+  Fidel.prototype.one = function(eventName, cb) {
+    this.el.one(eventName+this.namespace, cb);
+  };
+
+  Fidel.prototype.emit = function(eventName, data, namespaced) {
+    var ns = (namespaced) ? this.namespace : '';
+    this.el.trigger(eventName+ns, data);
+  };
+
+  Fidel.prototype.hide = function() {
+    if (this.views) {
+      for (var key in this.views) {
+        this.views[key].hide();
+      }
+    }
+    this.el.hide();
+  };
+  Fidel.prototype.show = function() {
+    if (this.views) {
+      for (var key in this.views) {
+        this.views[key].show();
+      }
+    }
+    this.el.show();
+  };
+
+  Fidel.prototype.destroy = function() {
+    this.el.empty();
+    this.emit('destroy');
+    this.el.unbind(this.namespace);
+  };
+
+  Fidel.declare = function(obj) {
+    var FidelModule = function(el, options) {
+      this.__init(el, options);
+    };
+    FidelModule.prototype = new Fidel(obj);
+    return FidelModule;
+  };
+
+  //for plugins
+  Fidel.onPreInit = function(fn) {
+    $('body').on('FidelPreInit', function(e, obj) {
+      fn.call(obj);
+    });
+  };
+  Fidel.onPostInit = function(fn) {
+    $('body').on('FidelPostInit', function(e, obj) {
+      fn.call(obj);
+    });
+  };
+  w.Fidel = Fidel;
+})(window, window.jQuery || window.Zepto);
+
+(function($) {
+  $.declare = function(name, obj) {
+
+    $.fn[name] = function() {
+      var args = Array.prototype.slice.call(arguments);
+      var options = args.shift();
+      var methodValue;
+      var els;
+
+      els = this.each(function() {
+        var $this = $(this);
+
+        var data = $this.data(name);
+
+        if (!data) {
+          var View = Fidel.declare(obj);
+          var opts = $.extend({}, options, { el: $this });
+          data = new View(opts);
+          $this.data(name, data); 
+        }
+        if (typeof options === 'string') {
+          methodValue = data[options].apply(data, args);
+        }
+      });
+
+      return (typeof methodValue !== 'undefined') ? methodValue : els;
+    };
+
+    $.fn[name].defaults = obj.defaults || {};
+
+  };
+
+  $.Fidel = window.Fidel;
+
+})(jQuery);
+
+(function($) {
+  $.declare('formjax', {
+
+    defaults: {
+      submitButton: '[type=submit]',
+      progressClass: 'btn-progress',
+      successClass: 'btn-success',
+      errorClass: 'btn-error',
+      progressText: 'Sending...',
+      successText: 'Done!',
+      errorText: 'Uh oh',
+      validate: function() {
+        //do some validation, if return false, don't submit, if true, submit
+        return true;
+      },
+      success: function(response) {
+        //fired when form response comes back
+      },
+      error: function(response) {
+        //fired when form errored out
+      }
+    },
+
+    events: {
+      'submit': 'submit'
+    },
+
+    init: function() {
+      this.button = this.el.find(this.submitButton);
+    },
+
+    submit: function(e) {
+      e.preventDefault();
+      if(this.validate()) {
+        this.button
+          .addClass(this.progressClass)
+          .val(this.progressText);
+
+        $.post(this.el.attr('action'), this.el.serialize())
+          .done(this.proxy(this._success))
+          .fail(this.proxy(this._error));
+      }
+    },
+
+    _success: function(response) {
+      this.button
+        .removeClass(this.progressClass)
+        .addClass(this.successClass)
+        .val(this.successText);
+      this.emit('ajaxify:success', response);
+      this.success.call(this.el, response);
+    },
+
+    _error: function(response) {
+      this.button
+        .removeClass(this.progressClass)
+        .addClass(this.errorClass)
+        .val(this.errorText);
+      $.notice(response.responseJSON.message, { level: 'error' });
+      this.emit('ajaxify:error', response);
+      this.error.call(this.el, response);
+    }
+  });
+})(jQuery);
